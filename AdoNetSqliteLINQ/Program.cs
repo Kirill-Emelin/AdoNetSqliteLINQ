d511614732814deb73ec326242a4193eea350e8b
Kirill-Emelin <128322099+Kirill-Emelin@users.noreply.github.com>
2024-08-27 20:10:57 +0200

    first commit

--- AdoNetSqliteLINQ/Program.cs
+++ AdoNetSqliteLINQ/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace AdoNetSqliteLINQ
+﻿using Microsoft.Data.Sqlite;
+
+namespace AdoNetSqliteLINQ
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string sqlExpression = "SELECT CustomerID, CompanyName, ContactName FROM Customers";
+            using (var connection = new SqliteConnection("DataSource=NorthwindSQLite.sqlite"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) 
+                    {
+                        while (reader.Read())   
+                        {
+                            var CustomerID = reader.GetValue(0);
+                            var CompanyName = reader.GetValue(1);
+                            var ContactName = reader.GetValue(2);
+
+                            Console.WriteLine($"{CustomerID} \t {CompanyName} \t {ContactName}");
+                        }
+                    }
+                }
+            }
+            Console.Read();
         }
     }
 }
